c4622588da9bc5f8c62681bff9ffb61f5b621c94
Philip Deaton <philipdeaton826@gmail.com>
2026-09-13 22:10:35 -0400

    individual symbol timing

--- include/Time.h
+++ include/Time.h
@@ -4,12 +4,33 @@
 
 #include <cstdint>
 
-class Time 
+#include "MorseSymbol.h"
+
+class Time
 {
-private:
-    const uint16_t m_btuMs;
-    uint32_t m_startTime;
-public:
-    Time(uint16_t value = 132);
-    uint32_t getTimestampMs() const;
+    private:
+
+        const uint32_t m_btuMs;
+
+        struct Durations
+        {
+            uint32_t ditMs;
+            uint32_t dahMs;
+            uint32_t symbolGapMs;
+            uint32_t letterGapMs;
+            uint32_t wordGapMs;
+        };
+        const Durations m_durationsMs;
+        
+        uint32_t m_symbolCompletionMs = 0;
+
+        uint32_t getTimestampMs() const;
+        uint32_t getDurationMs(Symbol symbol) const;
+
+    public:
+        Time(uint32_t btuMs = 132);
+
+        void newSymbol(Symbol symbol);
+        bool isSymbolComplete(Symbol symbol);
+
 };
--- src/Time.cpp
+++ src/Time.cpp
@@ -4,13 +4,46 @@
 
 #include <Arduino.h>
 
-Time::Time(uint16_t btuMs) 
+Time::Time(uint32_t btuMs)
 : m_btuMs(btuMs)
+, m_durationsMs{
+    btuMs,
+    static_cast<uint32_t>(3 * btuMs),
+    btuMs,
+    static_cast<uint32_t>(3 * btuMs),
+    static_cast<uint32_t>(7 * btuMs)
+}
 {
+}
 
+uint32_t Time::getDurationMs(Symbol symbol) const
+{
+    switch (symbol)
+    {
+        case Symbol::Dit:       return m_durationsMs.ditMs;
+        case Symbol::Dah:       return m_durationsMs.dahMs;
+        case Symbol::SymbolGap: return m_durationsMs.symbolGapMs;
+        case Symbol::LetterGap: return m_durationsMs.letterGapMs;
+        case Symbol::WordGap:   return m_durationsMs.wordGapMs;
+    }
+    return 0;
 }
 
 uint32_t Time::getTimestampMs() const
 {
     return static_cast<uint32_t>(millis());
+}
+
+void Time::newSymbol(Symbol symbol)
+{
+    m_symbolCompletionMs = getTimestampMs() + getDurationMs(symbol);
+}
+
+bool Time::isSymbolComplete(Symbol symbol)
+{
+    if (getTimestampMs() > m_symbolCompletionMs)
+    {
+        return true;
+    }
+    return false;
 }
